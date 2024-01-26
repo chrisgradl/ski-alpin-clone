869557dd1a65be4660c0a003afca81df8c48fffc
cgradl <christian.gradl@icloud.com>
2024-01-26 15:14:25 +0100

    added push notifications

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import * as Linking from 'expo-linking';
-import { Alert, Text } from 'react-native';
+import { Alert, Platform } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
 import {DefaultTheme, Link, NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from './src/screens/HomeScreen';
@@ -12,6 +14,16 @@ import AtheleteDetailScreen from './src/screens/AtheleteDetailScreen';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import CupRankingsScreen from './src/screens/CupRankingsScreen';
+
+
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 const Tab = createBottomTabNavigator();
 
@@ -86,18 +98,90 @@ const linking = {
   config: {
     /* configuration for matching screens with paths */
   },
+  async getInitialURL() {
+    // Check if app was opened from a deep link
+    const url = await Linking.getInitialURL();
+
+    if (url != null) {
+      return url;
+    }
+
+    // Check if there is an initial firebase notification
+    const message = await Notifications.getLastNotificationResponseAsync();
+    console.log(message)
+
+    // Get the `url` property from the notification which corresponds to a screen
+    // This property needs to be set on the notification payload when sending it
+    return message?.data?.url;
+  },
 };
+
+async function registerForPushNotificationsAsync() {
+  let token;
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+
+    try {
+      const firebaseToken = await Notifications.getDevicePushTokenAsync();
+      console.log(firebaseToken);
+    } catch (e) {
+      console.log(e)
+    }
+
+    // token = await Notifications.getExpoPushTokenAsync({
+    //   projectId: Constants.expoConfig.extra.eas.projectId,
+    // });
+
+
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+}
 
 export default function App() {
 
-  const url = Linking.useURL();
-
+  const [expoPushToken, setExpoPushToken] = React.useState('');
+  const [notification, setNotification] = React.useState(false);
+  const notificationListener = React.useRef();
+  const responseListener = React.useRef();
 
   React.useEffect(() => {
-    // if(url) {
-    //   Alert.alert(url);
-    // }
-  }, [url]);
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+      Alert.alert(JSON.stringify(notification));
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+      Alert.alert(JSON.stringify(notification));
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
 
   return (
     <QueryClientProvider client={queryClient}>
